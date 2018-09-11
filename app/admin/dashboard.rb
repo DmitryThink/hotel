6da30febacd7855cfe412fd5bda7afea90f53c36
Dmytro Kuzhel <Dmytro.Kuzhel@besidethepark.com>
2018-09-11 16:08:15 +0200

    Add clicks to admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc{ "Заработано: #{ Reservation.total_amount.to_i.to_s } грн.\n Посещений: #{ Click.count }" }
+  menu priority: 1, label: proc{ "Заработано: #{ Reservation.total_amount.to_i.to_s } грн.\n Посещений: #{ Click.where(location: "book").count }.\n Кликов: #{ Click.count }." }
 end
